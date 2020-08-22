aa1d0335b743cfd0ac086bf019e8b352333469af
ullaskunder <ullaskunder.work@gmail.com>
2020-08-22 19:44:29 +0530

    css added

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         <button
-          style={style}
+          className='button'
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
